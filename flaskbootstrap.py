f2182f454fac9b09cb460aff3bcc90ee87c7692f
Lucca Zovedi <luccazovedi@gmail.com>
2025-09-18 00:08:54 +0000

    Aula 050.B

--- flaskbootstrap.py
+++ flaskbootstrap.py
@@ -1,9 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 from flask_wtf import FlaskForm
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, SelectField, PasswordField
 from wtforms.validators import DataRequired
 
 app = Flask(__name__)
@@ -37,15 +37,46 @@ def request_context(name):
 # Formulário
 class NomeForm(FlaskForm):
     nome = StringField('Nome', validators=[DataRequired()])
+    sobrenome = StringField('Sobrenome', validators=[DataRequired()])
+    instituicao = StringField('Instituição', validators=[DataRequired()])
+    disciplina = SelectField('Disciplina', choices=[
+        ('', 'Escolha uma Disciplina'),
+        ('DSWA5', 'DSWA5'),
+        ('DWBA4', 'DWBA4'),
+        ('Gestão de Projetos', 'Gestão de Projetos')
+    ], validators=[DataRequired()])
     submit = SubmitField('Enviar')
 
 @app.route('/forms', methods=['GET', 'POST'])
 def forms():
     form = NomeForm()
-    nome = 'Stranger'
+    nome = sobrenome = instituicao = disciplina = ''
+
     if form.validate_on_submit():
         nome = form.nome.data
-    return render_template('forms.html', form=form, nome=nome)
+        sobrenome = form.sobrenome.data
+        instituicao = form.instituicao.data
+        disciplina = form.disciplina.data
+
+    return render_template('forms.html', form=form, nome=nome, sobrenome=sobrenome, instituicao=instituicao, disciplina=disciplina)
+
+# Login
+class LoginForm(FlaskForm):
+    usuario = StringField('Usuário ou E-mail', validators=[DataRequired()])
+    senha = PasswordField('Senha', validators=[DataRequired()])
+    submit = SubmitField('Entrar')
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        usuario = form.usuario.data
+        return redirect(url_for('loginResponse', usuario=usuario))
+    return render_template('login.html', form=form)
+
+@app.route("/loginResponse/<usuario>")
+def loginResponse(usuario):
+    return render_template('loginResponse.html', usuario=usuario)
 
 # Executar
 if __name__ == '__main__':
